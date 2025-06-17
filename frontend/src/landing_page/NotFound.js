9d35c7f3f823758e06821ec2acb797dd261b82ab
devvarth Singh <devvarthsinghwork@gmail.com>
2025-06-17 13:43:14 +0530

    Re-added dashboard and frontend as regular folders

---
+++ frontend/src/landing_page/NotFound.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function NotFound() {
+    return ( 
+        <div className='container p-5'>
+        <div className='row text-center'>
+                
+                <h1 className='mt-5'>404 Not Found</h1> 
+                <p>
+               sorry the page you are looking fore does not exist
+                </p>  
+               
+        </div>
+    </div>
+     );
+}
+
+export default NotFound;
